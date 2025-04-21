989e4d2e0e90608666c6a1e94fb4c64bca797112
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2025-04-21 10:05:54 +0530

    creating pages and adding header components to home page

--- cara e-commerce/shopping-app/src/App.jsx
+++ cara e-commerce/shopping-app/src/App.jsx
@@ -1,11 +1,15 @@
 import Navbar from "./components/navbar/Navbar"
-
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/home/Home"
 
 function App() {
 
   return (
     <>
      <Navbar/>
+     <Routes>
+      <Route path="/" element={<Home/>}/>
+     </Routes>
     </>
   )
 }
